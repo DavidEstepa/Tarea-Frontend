e669692c7473d6125fcbf2b6dbc7ef1fdf5c1963
DavidEstepa <depestepa@gmail.com>
2020-05-21 10:42:58 -0500

    Se agrega la funcionalidad completa de entrada y salida del sistema

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-body',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private loggingService: LoggingService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  salida(){
+    let state: boolean;
+    state = this.loggingService.confirm('', '');
+    if (!state){
+      this.router.navigateByUrl('/ingreso');
+    }
+  }
 }
