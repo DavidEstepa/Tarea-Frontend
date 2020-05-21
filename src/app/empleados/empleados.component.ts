bd8867bc527f12dbd98d700ec5d0417eb0fa9368
DavidEstepa <depestepa@gmail.com>
2020-05-21 16:55:58 -0500

    Se agrega el componente de ingresar empleados

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-empleados',
   templateUrl: './empleados.component.html',
   styleUrls: ['./empleados.component.css']
 })
 export class EmpleadosComponent implements OnInit {
-
-  constructor() { }
+  public EmpleadosForm: FormGroup;
+  constructor(
+    private formBulder: FormBuilder
+    ) {
+      this.EmpleadosForm = this.formBulder.group({
+        nombres: ['', Validators.required],
+        apellidos: ['', Validators.required],
+        documento: ['', Validators.required],
+        edad: ['', Validators.required],
+        correo: ['', Validators.required],
+        direccion: ['', Validators.required],
+        telefono: ['', Validators.required],
+        eps: ['', Validators.required],
+      });
+    }
 
   ngOnInit(): void {
   }
-
+  onSubmit(){
+    console.log('Holi');
+  }
 }
